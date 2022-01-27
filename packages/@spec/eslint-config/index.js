980908cb537f55cd9e4e13792d1d4669837e5b45
zhengxs <zhenxs2018@foxmail.com>
2022-01-27 10:12:36 +0800

    修改模块名称

--- packages/@spec/eslint-config/index.js
+++ packages/@spec/eslint-config/index.js
@@ -15,7 +15,10 @@ const config = {
     // 不进行强制的 null 检查
     // 否则所有使用 ! 的地方都无法使用
     '@typescript-eslint/no-non-null-assertion': 'off',
+    // 正在想办法解决 unknown 无法代替 any 的问题
+    '@typescript-eslint/no-explicit-any': 'off',
     'tsdoc/syntax': 'warn',
+    // 防止意外导入
     'import/no-extraneous-dependencies': [
       'error',
       {
